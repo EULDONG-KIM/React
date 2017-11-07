d236502355c244fb29e56f25d8203ab3107b4c23
EULDONG-KIM <ked9181@naver.com>
2017-11-07 18:40:50 +0900

    [commit 테스트]
    - 개행문자 테스트 완료 후 원복

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './components/App'; 
 
 const rootElement = document.getElementById('root');
-/* commit 개행 문자 테스트 */
+
 ReactDOM.render( < App / > , rootElement);
